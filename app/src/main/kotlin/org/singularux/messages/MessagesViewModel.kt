5777d68d1317651ce8cfccffbf53dee24e400061
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-05-20 12:21:38 +0200

    Started adding observers for data

--- app/src/main/kotlin/org/singularux/messages/MessagesViewModel.kt
+++ app/src/main/kotlin/org/singularux/messages/MessagesViewModel.kt
@@ -1,6 +1,7 @@
 package org.singularux.messages
 
 import android.app.Application
+import android.provider.Telephony
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,13 +9,17 @@ import androidx.lifecycle.MutableLiveData
 class MessagesViewModel(app: Application) : AndroidViewModel(app) {
 
     private val mutableThreads = MutableLiveData<List<ThreadItem>>()
+    private val threadsObserver = ThreadsObserver(app.applicationContext, mutableThreads)
 
     fun onInit() {
-
+        getApplication<Application>().contentResolver.registerContentObserver(
+            Telephony.MmsSms.CONTENT_CONVERSATIONS_URI, true, threadsObserver
+        )
+        threadsObserver.dispatchChange(false, Telephony.MmsSms.CONTENT_CONVERSATIONS_URI)
     }
 
     override fun onCleared() {
-
+        getApplication<Application>().contentResolver.unregisterContentObserver(threadsObserver)
     }
 
     val threads: LiveData<List<ThreadItem>>
---
+++ app/src/main/kotlin/org/singularux/messages/ThreadsObserver.kt
@@ -0,0 +1,44 @@
+package org.singularux.messages
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.Telephony
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class ThreadsObserver(
+    private val context: Context,
+    private val mutableThreads: MutableLiveData<List<ThreadItem>>
+) : ContentObserver(Handler(Looper.getMainLooper())) {
+
+    companion object {
+        private val PROJECTION = arrayOf(Telephony.MmsSms._ID)
+    }
+
+    private var job: Job? = null
+
+    override fun onChange(selfChange: Boolean) {
+        // Check for permissions
+        if (context.checkSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        // Execute
+        job?.cancel()
+        job = CoroutineScope(Dispatchers.IO).launch {
+
+        }
+    }
+
+    override fun onChange(selfChange: Boolean, uris: MutableCollection<Uri>, flags: Int) {
+        onChange(selfChange)  // Call only one time for API 30+
+    }
+
+}
