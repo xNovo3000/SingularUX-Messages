082ca20fcc65a535ec09538c4cb392ddcebe68e9
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-05-20 09:41:35 +0200

    Created basic routes

--- app/src/main/kotlin/org/singularux/messages/ui/MessagesUI.kt
+++ app/src/main/kotlin/org/singularux/messages/ui/MessagesUI.kt
@@ -1,12 +1,27 @@
 package org.singularux.messages.ui
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.singularux.messages.ui.route.SearchRoute
+import org.singularux.messages.ui.route.ThreadsRoute
 import org.singularux.messages.ui.theme.MessagesTheme
+
+sealed class Route(val name: String) {
+    object Threads : Route("threads")
+    object Search : Route("search")
+}
 
 @Composable
 fun MessagesUI() {
     // Apply the theme globally
     MessagesTheme {
-        // TODO: Build the app
+        // Add the navigation component
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = Route.Threads.name) {
+            composable(Route.Threads.name) { ThreadsRoute(navController = navController) }
+            composable(Route.Search.name) { SearchRoute(navController = navController) }
+        }
     }
 }
---
+++ app/src/main/kotlin/org/singularux/messages/ui/route/ThreadsRoute.kt
@@ -0,0 +1,11 @@
+package org.singularux.messages.ui.route
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ThreadsRoute(
+    navController: NavHostController
+) {
+
+}
