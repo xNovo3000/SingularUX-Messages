99bb5b6b40b6de41ac032b41d65ef6709ec63ee9
xNovo3000 <fontana.ciro.2001@gmail.com>
2022-05-21 19:16:54 +0200

    Added proguard and animation navigation

--- app/src/main/kotlin/org/singularux/messages/ui/MessagesUI.kt
+++ app/src/main/kotlin/org/singularux/messages/ui/MessagesUI.kt
@@ -1,11 +1,14 @@
 package org.singularux.messages.ui
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import org.singularux.messages.ui.route.SearchRoute
 import org.singularux.messages.ui.route.ThreadsRoute
@@ -19,14 +22,15 @@ sealed class Route(val name: String) {
 
 @ExperimentalPermissionsApi
 @ExperimentalFoundationApi
+@ExperimentalAnimationApi
 @ExperimentalMaterial3Api
 @Composable
 fun MessagesUI() {
     // Apply the theme globally
     MessagesTheme {
         // Add the navigation component
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = Route.Threads.name) {
+        val navController = rememberAnimatedNavController()
+        AnimatedNavHost(navController = navController, startDestination = Route.Threads.name) {
             composable(Route.Threads.name) { ThreadsRoute(navController = navController) }
             composable(Route.Search.name) { SearchRoute(navController = navController) }
         }
